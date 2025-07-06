37e6ca3dccad5b2e198b3e061c696086ed291d32
focusflarenetwork <service@focusflarenetwork.com>
2025-07-06 15:04:44 -0400

    added a bunch of stuff

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+// Use CommonJS syntax for compatibility
+const express = require('express');
+
+const app = express();
+
+app.listen(5000, () => {
+    console.log('Server started at http://localhost:5000 hello world');
+});
